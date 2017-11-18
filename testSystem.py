f021b29b5b2ac65e8a313c8e54b0f600f178d038
Canta Tronic <tokareva@jinr.ru>
2017-11-19 01:52:37 +0300

    Add a kind of hande-made mock for Marssystem - testSystem.py

---
+++ testSystem.py
@@ -0,0 +1,10 @@
+
+class MarsSystem:
+    def __init__(self):
+        print "system init"
+
+    def finalise(self):
+        print "system finalize"
+        
+if __name__ == "__main__":
+    pass
